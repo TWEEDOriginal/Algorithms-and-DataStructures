12d9ca6d7da69b55cb26d2ab0aa69ca743e4db45
Tawheed <tawheed@Tawheeds-MacBook-Pro-2.local>
2022-09-02 12:32:07 +0100

    feat: solution by creating a 'big items list' CTCI Q2C4

--- linked_lists/Partition/partition.js
+++ linked_lists/Partition/partition.js
@@ -1,3 +1,15 @@
+/**
+ * puts elements less than x on the left
+ * and other elements on the right side
+ *
+ * N = size of linkedlist
+ * Time: O(N)
+ * Additional space: O(N)
+ *
+ * @param  {array}  int[] - linkedlist items
+ * @return {array}   int[] - partitioned array
+ */
+
 import { LinkedList, Node } from "../helpers/linkedList.js";
 
 const partition = (arr, x) => {
@@ -35,5 +47,10 @@ const partition = (arr, x) => {
   return list.toArray();
 };
 
-console.log(partition([3, 5, 8, 5, 10, 2, 1], 5));
-console.log(partition([5, 5, 8, 5, 10, 3, 2, 1], 5));
+console.log(partition([3, 5, 8, 5, 10, 2, 1], 5), [3, 2, 1, 5, 8, 5, 10]);
+console.log(partition([5, 5, 8, 5, 10, 3, 2, 1], 5), [3, 2, 1, 5, 5, 8, 5, 10]);
+console.log(partition([5, 8, 7, 4, 9, 15, 30], 31), [5, 8, 7, 4, 9, 15, 30]);
+console.log(
+  partition([4, 9, 15, 30, 5, 8, 3, 2, 2, 3, 7, 1, 15, 5, 6, 7], 6),
+  [4, 5, 3, 2, 2, 3, 1, 5, 9, 15, 30, 8, 7, 15, 6, 7]
+);
