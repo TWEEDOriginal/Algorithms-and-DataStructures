8e70b2ec9ee7706a9503f5de43d911a9ccf3149c
Tawheed <tawheed@Tawheeds-MacBook-Pro-2.local>
2022-10-02 21:46:43 +0100

    feat: breadth first search

--- trees_and_graphs/helpers/GraphSearch/breadthFirstSearch.js
+++ trees_and_graphs/helpers/GraphSearch/breadthFirstSearch.js
@@ -7,12 +7,13 @@
  * Time: O(N^d)
  * Additional space: O(N)
  *
- * @param  {node} Vertex - root node to search from
+ * @param  {Node} Vertex - root node to search from
  * @return {set} - set containing all found nodes
  */
 
 import { Queue } from "../../../stacks_and_queues/helpers/queue.js";
-import { Graph, UNDIRECTED } from "../graph.js";
+import { Graph, UNDIRECTED, Node } from "../graph.js";
+import { LinkedList } from "../../../linked_lists/helpers/linkedList.js";
 
 export const search = (root) => {
   if (!root) return;
@@ -35,21 +36,59 @@ export const search = (root) => {
   return visited;
 };
 
-// const graph = new Graph(UNDIRECTED);
+export const breadthFirstSearch = (start, end) => {
+  if (!start || !end) return;
+  let found;
+  const path = new LinkedList();
+  const visitList = new Queue();
+  start.previous = null;
+  visitList.add(start);
+  let node;
 
-// const [root] = graph.addEdge(1, 2);
-// graph.addEdge(1, 3);
-// graph.addEdge(1, 4);
-// graph.addEdge(5, 2);
-// graph.addEdge(6, 3);
-// graph.addEdge(7, 3);
-// graph.addEdge(8, 4);
-// graph.addEdge(9, 5);
-// graph.addEdge(10, 6);
+  while (!visitList.isEmpty()) {
+    node = visitList.remove();
 
+    if (node === end) {
+      found = node;
+      break;
+    }
+
+    for (let adj of node.getAdjacents()) {
+      if (adj && adj.previous === undefined) {
+        adj.previous = node;
+        visitList.add(adj);
+      }
+    }
+  }
+  let temp;
+  while (found) {
+    path.prepend(found.value);
+    temp = found.previous;
+    found.previous = undefined;
+    found = temp;
+  }
+  return path.toArray();
+};
+
+const graph = new Graph(UNDIRECTED);
+
+const [start] = graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(1, 4);
+graph.addEdge(5, 2);
+graph.addEdge(6, 3);
+graph.addEdge(7, 3);
+graph.addEdge(8, 4);
+graph.addEdge(9, 5);
+graph.addEdge(10, 6);
+graph.addEdge(2, 11);
+const [end] = graph.addEdge(20, 11);
 // let values = [];
 
-// for (let node of search(root)) {
+// for (let node of search(start)) {
 //   values.push(node.value);
 // }
 // console.log(values, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+console.log(breadthFirstSearch(start, end));
+console.log(breadthFirstSearch(start, new Node(10)));
